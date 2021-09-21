705d5836d20a029aca16c6f645736c66cefd9125
Anaelle693 <anaellev@icloud.com>
2021-09-21 15:03:25 +0200

    Reussite navbar et redirection

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -34,7 +34,7 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_login');
+            return $this->redirectToRoute('article_index');
         }
 
         return $this->render('registration/register.html.twig', [
--- src/Security/LoginFromAuthenticator.php
+++ src/Security/LoginFromAuthenticator.php
@@ -51,7 +51,7 @@ class LoginFromAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('app_login'));
+        return new RedirectResponse($this->urlGenerator->generate('article_index'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
