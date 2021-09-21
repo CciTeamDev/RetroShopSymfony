70d9706fcb92738304869d22105b0221c0652ca2
B0UBA <moussierquentin@gmail.com>
2021-09-21 09:03:18 +0200

    search done

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -68,6 +68,20 @@ class ArticleController extends AbstractController
         ]);
     }
 
+   
+
+    #[Route('/search', name: 'article_search', methods: ['POST'])]
+    public function search(ArticleRepository $articleRepository,Request $request): Response
+    {
+
+
+        return $this->render('article/index.html.twig', [
+            'articles' => $articleRepository->search($request->get('terme'))
+           
+        ]);
+
+    }
+
     #[Route('/{id}', name: 'article_delete', methods: ['POST'])]
     public function delete(Request $request, Article $article): Response
     {
@@ -79,4 +93,5 @@ class ArticleController extends AbstractController
 
         return $this->redirectToRoute('article_index', [], Response::HTTP_SEE_OTHER);
     }
+    
 }
