380f988b007cd0d7ecb1dfdf676a872c53d28c19
Windryuu <brayan.jean@laposte.net>
2021-09-22 16:24:31 +0200

    Panier full BDD ok

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,35 +2,49 @@
 
 namespace App\Controller;
 
+use App\Entity\Purchase;
 use App\Entity\PurchaseHaveProduct;
+use App\Entity\User;
 use App\Service\Cart\CartService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/user')]
 class CartController extends AbstractController
 {
     #[Route('/cart', name: 'cart_index')]
-    public function index(CartService $cartService): Response
+    public function index(CartService $cartService): Response //ok
     {
+        $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['id'=>$this->getUser()->getId()]);
+        $purchase = $this->getDoctrine()->getRepository(Purchase::class)->findOneBy(['user'=>$user,'status'=>'panier']);
+        //dd($cartService->getFullCart($purchase));
         return $this->render('cart/index.html.twig', [
             'controller_name' => 'CartController',
-            'items' => $cartService->getFullCart(),
-            'total' => $cartService->getTotal()
+            'items' => $cartService->getFullCart($purchase),
+            'total' => $cartService->getTotal($purchase)
         ]);
     }
 
     #[Route('/cart/add/{id}', name:"cart_add")]
-    public function add($id,CartService $cartService,Request $request){
-        $cartService->add($id,$request->get('quantity'));
+    public function add($id,CartService $cartService,Request $request){ //ok
+
+        $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['id'=>$this->getUser()->getId()]);
+        $purchase = $this->getDoctrine()->getRepository(Purchase::class)->findOneBy(['user'=>$user,'status'=>'panier']);
+
+        $cartService->add($id,$request->get('quantity'),$purchase);
         
         return $this->redirectToRoute('cart_index');
     }
 
     #[Route('/cart/remove/{id}', name:"cart_remove")]
-    public function remove($id, CartService $cartService){
-        $cartService->remove($id);
+    public function remove($id, CartService $cartService){ //ok
+
+        $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['id'=>$this->getUser()->getId()]);
+        $purchase = $this->getDoctrine()->getRepository(Purchase::class)->findOneBy(['user'=>$user,'status'=>'panier']);
+
+        $cartService->remove($id,$purchase);
         return $this->redirectToRoute("cart_index");
     }
 
--- src/Service/Cart/CartService.php
+++ src/Service/Cart/CartService.php
@@ -2,73 +2,88 @@
 
 namespace App\Service\Cart;
 
+use App\Entity\Article;
 use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use App\Entity\PurchaseHaveProduct as Cart;
+use App\Entity\Purchase;
 
 class CartService{
 
     protected $session;
     protected $articleRepository;
+    private $entityManager;
 
-    public function __construct(SessionInterface $session,ArticleRepository $articleRepository)
+    public function __construct(SessionInterface $session,ArticleRepository $articleRepository,$entityManager)
     {
+        $this->entityManager = $entityManager;
         $this->session = $session;
         $this->articleRepository = $articleRepository;
     }
 
-    public function add(int $id,int $qte) {
+    public function add(int $id,int $qte,Purchase $purchase) { //ok
 
-        $panier = $this->session->get('panier',[]);
-        if(!empty($panier[$id])){
-            $panier = $this->update($id,$qte);
-        } else {
-            $panier[$id] = $qte;
-        }
-        $this->session->set('panier',$panier);
-    }
-
-    public function update(int $id,int $qte) {
-    
-        $panier = $this->session->get('panier',[]);
-        if(!empty($panier[$id])){
-            $panier[$id]+=$qte;
-        } else {
-            $error_message = false;
-            return $error_message;
-        }
-        return $panier;
-    }
-
-    public function remove(int $id){
-        $panier = $this->session->get('panier',[]);
-        if(!empty($panier[$id])){
-            unset($panier [$id]);
-        }
-
-        $this->session->set('panier',$panier);
-
-    }
-
-    public function getFullCart():array{
-        $panier = $this->session->get('panier',[]);
-        $panierWithData = [];
+        $art = $this->entityManager->getRepository(Article::class)->findOneBy(['id'=>$id]);
+        $cart = $this->entityManager->getRepository(Cart::class)->findBy(['purchase'=>$purchase,'article'=>$art]);
         
-        foreach($panier as $id => $quantity){
+        if($cart === []){
+            //dd('boop');
+            $em = $this->entityManager;
+            
+            $cart = new Cart();
+            $cart->setPurchase($purchase);
+            $cart->setQuantity($qte);
+            $cart->setArticle($art);
+
+            $em->persist($cart); 
+            $em->flush();
+        } else {
+            //dd('beep');
+            $this->update($id,$qte,$purchase);
+        }
+        
+    }
+
+    public function update(int $id,int $qte,Purchase $purchase) { //ok
+        
+        $art = $this->entityManager->getRepository(Article::class)->findOneBy(['id'=>$id]);
+        $cart = $this->entityManager->getRepository(Cart::class)->findBy(['purchase'=>$purchase,'article'=>$art]);
+        //dd($art,$cart);
+        $em = $this->entityManager;
+        $cart = $em->getRepository(Cart::class)->findOneBy(['purchase'=>$purchase,'article'=>$art]);
+        $qte = $cart->getQuantity() + $qte;
+        $cart->setQuantity($qte);
+        $em->flush();
+    
+    }
+
+    public function remove(int $id,Purchase $purchase){ //ok
+        $art = $this->entityManager->getRepository(Article::class)->findOneBy(['id'=>$id]);
+        $panier = $this->entityManager->getRepository(Cart::class)->findOneBy(['purchase'=>$purchase,'article'=>$art]);
+        $this->entityManager->remove($panier);
+        $this->entityManager->flush();
+    }
+
+    public function getFullCart(Purchase $purchase){ //ok
+        $panierWithData =[];
+        $panier = $this->entityManager->getRepository(Cart::class)->findBy(['purchase'=>$purchase]);
+        $art = $this->entityManager->getRepository(Article::class)->findAll();
+        
+        foreach($panier as $subPanier){
             $panierWithData[] = [
-                'product' => $this->articleRepository->find($id),
-                'quantity'=>$quantity
+                'product' => $subPanier->getArticle(),
+                'quantity'=> $subPanier->getQuantity()
             ];
         }
         return $panierWithData;
     }
 
-    public function getTotal():float{
+    public function getTotal(Purchase $purchase):float{ //ok
         $total = 0;
-        
-
-        foreach($this->getFullCart() as $item){
+        foreach($this->getFullCart($purchase) as $item){
             $total += $item['product']->getPrice() * $item['quantity'];
         }
         return $total;
     }
+
 }
