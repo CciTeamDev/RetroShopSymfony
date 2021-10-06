98b846e2a44dcfb4f3b3b24744f74c7a6317b87a
B0UBA <moussierquentin@gmail.com>
2021-10-06 12:15:22 +0200

    bug stipe

--- src/Form/AdresseType.php
+++ src/Form/AdresseType.php
@@ -3,12 +3,14 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CountryType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
 
 class AdresseType extends AbstractType
 {
@@ -19,19 +21,31 @@ class AdresseType extends AbstractType
                 'label' => 'Quel nom souhaitez-vous donner à votre adresse ?',
                 'attr' => [
                     'placeholder' => 'Nommez votre adresse'
-                ]
+                ],
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 30
+                ])
             ])
             ->add('firstname', TextType::class, [
                 'label' => 'Votre prénom',
                 'attr' => [
                     'placeholder' => 'Entrez votre prénom'
-                ]
+                ],
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 30
+                ])
             ])
             ->add('lastname', TextType::class, [
                 'label' => 'Votre nom',
                 'attr' => [
                     'placeholder' => 'Entre votre nom'
-                ]
+                ],
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 30
+                ])
             ])
             ->add('company', TextType::class, [
                 'label' => 'Votre société',
@@ -46,11 +60,17 @@ class AdresseType extends AbstractType
                     'placeholder' => '8 rue des lylas ...'
                 ]
             ])
-            ->add('cp', TextType::class, [
+            ->add('cp', NumberType::class, [
                 'label' => 'Votre code postal',
                 'attr' => [
-                    'placeholder' => 'Entrez votre code postal'
-                ]
+                    'placeholder' => 'Entrez votre code postal',
+                    'min' => 01000,
+                    'max' => 99000
+                ],
+                'constraints' => new Length([
+                    'max' => 5
+                ])
+
             ])
             ->add('ville', TextType::class, [
                 'label' => 'Ville',
@@ -68,7 +88,12 @@ class AdresseType extends AbstractType
                 'label' => 'Votre téléphone',
                 'attr' => [
                     'placeholder' => 'Entrez votre téléphone'
-                ]
+                ],
+                'constraints' => new Length([
+                    'min' => 10,
+                    'max' => 10
+                ])
+
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Valider',
